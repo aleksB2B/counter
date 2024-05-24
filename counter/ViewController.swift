64e16ff3a923f7dd05dffb4fab8a339c3ee46c32
AleksM <aleks5721501@gmail.com>
2024-05-24 11:07:05 +0400

    Добавил изменения Кнопка и счетчик

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+
+    @IBOutlet weak var numberLable: UILabel!
+    @IBOutlet weak var switchButon: UIButton!
+
+    private var number = 0
+    private var text = "Значение счетчика:"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        numberLable.text = "\(text) \(number)"
+
+
     }
 
+    @IBAction func buttonTap(_ sender: Any) {
+        number += 1
+        numberLable.text = "\(text) \(number)"
 
+    }
+    
 }
 
